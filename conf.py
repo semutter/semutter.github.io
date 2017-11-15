a8b7b71e04ab5ac20e11093987a3ab93aad1c379
semut <bettersemut@gmail.com>
2017-11-15 20:14:31 +0800

    Nikola auto commit.
    Nikola version: 7.8.10

--- conf.py
+++ conf.py
@@ -1375,3 +1375,8 @@ COMPILERS["orgmode"] = ('.org',)
 # Add org files to your POSTS, PAGES
 POSTS = POSTS + (("posts/*.org", "posts", "post.tmpl"),)
 PAGES = PAGES + (("stories/*.org", "stories", "story.tmpl"),)
+
+
+# github config
+GITHUB_SOURCE_BRANCH = 'source'
+GITHUB_DEPLOY_BRANCH = 'master'
